3c2a61a2ed0408976bf4713769d1bedd652557b4
brtester2025-collab <brtester2025@gmail.com>
2026-02-09 17:52:02 +0530

    FOR Report file only

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    testMatch: ['**/tests/backend/**/*.test.js'],
+    reporters: [
+        'default',
+        [
+            'jest-html-reporters',
+            {
+                publicPath: './jest-report',
+                filename: 'report.html',
+                expand: true,
+            },
+        ],
+    ],  // only unit folder
+};
