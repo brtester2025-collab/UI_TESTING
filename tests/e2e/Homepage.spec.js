d8ca0263e33d7c2d06260263af6acfcd4dd82070
brtester2025-collab <brtester2025@gmail.com>
2026-02-24 13:14:11 +0530

    files

--- tests/e2e/Homepage.spec.js
+++ tests/e2e/Homepage.spec.js
@@ -3,6 +3,12 @@ import { test, expect } from '@playwright/test';
 
 test.describe('HomePage', () => {
     test.beforeEach(async ({ page }) => {
-        await page.goto('/')
+        await page.goto('https://www.saucedemo.com/inventory.html')
     })
+
+    test('should have correct title', async ({ page }) => {
+        await expect(page).toHaveTitle(/Swag Labs/);
+    })
+
+    test('should have visible hero section')
 })
--- tests/e2e/login.spec.js
+++ tests/e2e/login.spec.js
@@ -1,30 +1,32 @@
 import { test, expect, describe } from '@playwright/test';
 
 
-describe('Swagger Login Authentication', () => {
+test.describe('Login form', () => {
 
-
-    test.beforeEach('should login successfully and verify URL', async ({ page }) => {
-        await page.goto('https://www.saucedemo.com/');
-        await page.locator('[data-test="username"]').fill('standard_user');
-        await page.locator('[data-test="password"]').fill('secret_sauce');
-        await page.locator('[data-test="login-button"]').click();
-        await expect(page).toHaveURL(/inventory/);
-
-
-    });
-
-    test('to have the text for the product page', async ({ page }) => {
-        await expect(page.locator('.title')).toHaveText('Products')
-
+    test.beforeEach(async ({ page }) => {
+        await page.goto('https://www.saucedemo.com')
     })
 
-    test('to check the product page', async ({ page }) => {
-        const addtoCart = page.locator('[data-test="add-to-cart-sauce-labs-backpack"]')
-        const cartCount = page.locator('[data-test="shopping-cart-badge"]')
-        await addtoCart.click()
-        await expect(cartCount).toHaveText('1')
+    test('Test to contain all the elements', async ({ page }) => {
+        const logo = page.locator('.login_logo')
+        const username = page.locator('#user-name')
+        const password = page.locator('#password')
+        const loginButton = page.locator('#login-button')
+        await expect(logo).toBeVisible()
+        await expect(username).toBeVisible()
+        await expect(password).toBeVisible()
+        await expect(loginButton).toBeVisible()
     })
+
+    test('Test for the correct placeholder Test', async ({ page }) => {
+        await expect(page.locator('#username')).toHaveAttribute('placeholder', 'Username')
+        await expect(page.locator('#password')).toHaveAttribute('placeholder', 'password')
+    })
+
+
+
+
+
 
 
 
