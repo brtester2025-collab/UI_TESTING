bbc7d3e8d8f0bfa3859679b6c1f1cf48e7b560bf
brtester2025-collab <brtester2025@gmail.com>
2026-02-25 16:01:45 +0530

    automation L1

--- tests/e2e/product.spec.js
+++ tests/e2e/product.spec.js
@@ -13,11 +13,19 @@ test.describe('Product Page', () => {
 
     })
 
-    test('Checking the inventory count', async ({ page }) => {
-        const countItems = page.locator('#inventory_item')
+    test('Checking the Product count', async ({ page }) => {
+        const countItems = page.locator('.inventory_item')
+        await expect(countItems).toHaveCount(6)
 
-        await expect(countItems).toHaveCount(0)
+    })
 
+    test('checking the product has price,image and description', async ({ page }) => {
+
+        const firstProduct = await page.locator('.inventory_item').first()
+        await expect(firstProduct.locator('img.inventory_item_img')).toBeVisible()
+        await expect(firstProduct.locator('.inventory_item_description')).toBeVisible()
+        await expect(firstProduct.locator('.inventory_item_name ')).toBeVisible()
+        await expect(firstProduct.locator('.inventory_item_price')).toBeVisible()
     })
 
 
