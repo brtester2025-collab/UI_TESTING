8a4d7486479adaf720679c637cb249468e07aae9
brtester2025-collab <brtester2025@gmail.com>
2026-02-26 11:41:44 +0530

    Add unit test : for adding multiple products

--- tests/e2e/product.spec.js
+++ tests/e2e/product.spec.js
@@ -32,8 +32,52 @@ test.describe('Product Page', () => {
         prices.forEach((price) => {
             expect(price).toMatch(/^\$\d+\.\d{2}$/)
         })
+    })
+
+    test('Sorting of price from low to high', async ({ page }) => {
+
+        await page.locator('[data-test= "product-sort-container"]').selectOption('lohi')
+        const price = await page.locator('.inventory_item_price').allTextContents()
+        const numericPrice = price.map(p => parseFloat(p.replace('$', '')))
+
+        for (let i = 0; i < numericPrice.length - 1; i++) {
+            expect(numericPrice[i]).toBeLessThanOrEqual(numericPrice[i + 1])
+        }
+    })
+
+    test('Sorting the price from high to low', async ({ page }) => {
+        await page.locator('[data-test ="product-sort-container"]').selectOption('hilo')
+        const price = await page.locator('.inventory_item_price').allTextContents()
+        const numericPrice = price.map(p => parseFloat(p.replace('$', '')))
+
+        for (let i = 0; i < numericPrice.length - 1; i++) {
+            expect(numericPrice[i]).toBeGreaterThanOrEqual(numericPrice[i + 1])
+        }
+    })
+
+    test('Sort the product A-Z', async ({ page }) => {
+        await page.locator('[data-test ="product-sort-container"]').selectOption('az')
+        const names = await page.locator('.inventory_item_name').allTextContents()
+        const sort = [...names].sort();
+        expect(names).toEqual(sort)
+    })
+
+    test('To add the multiple products and check the cart count', async ({ page }) => {
+        await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click()
+        await page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]').click()
+        await page.locator('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click()
+
+
+        await expect(page.locator('.shopping_cart_badge')).toHaveText('3')
+    })
+
+    test('Cart badge is not visible when no product is added to the cart', async ({ page }) => {
+        await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click()
+        await page.locator('[data-test="remove-sauce-labs-backpack"]').click()
+        await expect(page.locator('.shopping_cart_badge')).not.toBeVisible()
 
     })
+
 
 
 })
