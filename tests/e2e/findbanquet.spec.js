b459074f42eb416e2ff71b2ba15e0a7047ccc2be
brtester2025-collab <brtester2025@gmail.com>
2026-03-10 18:33:15 +0530

    New case for unit testing with api

--- tests/e2e/findbanquet.spec.js
+++ tests/e2e/findbanquet.spec.js
@@ -1,17 +1,21 @@
 const { test, expect } = require('@playwright/test')
 
-test('test', async ({ page }) => {
-    await page.goto('https://demo-7.com/findbanquet/');
-    await page.getByRole('navigation').getByRole('link', { name: 'Mehndi' }).click();
-    await page.getByRole('navigation').getByRole('link', { name: 'Makeup Artist' }).click();
-    await page.getByRole('navigation').getByRole('link', { name: 'Banquet Hall' }).click();
-    await page.getByRole('navigation').getByRole('link', { name: 'Fashion' }).click();
-    await page.getByRole('navigation').getByRole('link', { name: 'Hotels' }).click();
-    await page.getByRole('navigation').getByRole('link', { name: 'Restaurants' }).click();
-    await page.getByRole('navigation').getByRole('link', { name: 'Photographer' }).click();
-    await page.getByRole('navigation').getByRole('link', { name: 'Decorators' }).click();
-    await page.getByRole('button', { name: 'Sign In' }).click();
-    await page.getByRole('textbox', { name: 'Mobile Number' }).click();
-    await page.getByRole('button', { name: 'Send OTP' }).click();
-    await page.getByRole('button', { name: 'Verify & Continue' }).click();
-});
+
+test.describe('Find banquet', () => {
+    test('Home Page', async ({ page }) => {
+        await page.goto('https://findbanquet.com/');
+        await expect(page).toHaveURL('https://findbanquet.com/')
+        await page.getByText('Anchors').click()
+        await expect(page).toHaveURL('https://findbanquet.com/vendors/anchors')
+    });
+
+    test('login page for user', async ({ page }) => {
+        await page.goto('https://findbanquet.com/');
+        await page.getByRole('button', { name: 'Sign In' }).click()
+        await expect(page).toHaveURL('https://findbanquet.com/login')
+    })
+
+    test('user registration for Vendor')
+
+})
+
