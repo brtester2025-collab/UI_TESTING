eefd5b1ca5ea505cdd721806cf54a5a0a56a3297
brtester2025-collab <brtester2025@gmail.com>
2026-03-23 16:07:52 +0530

    Add unit test for page

--- tests/e2e/home_page.spec.js
+++ tests/e2e/home_page.spec.js
@@ -19,6 +19,17 @@ test.describe('Main Page', () => {
 
     })
 
+
+    test('Categories header section', async ({ page }) => {
+        const categories = ['Mehendi', 'Makeup Artist', 'Makeup Artist', 'Banquet Hall', 'Fashion'
+            , 'Resort', 'Hotel', 'Restaurants', 'Photographer', 'Decorators']
+
+        for (let i of categories) {
+            await expect(page.getByAltText(`${i}`).first()).toBeVisible()
+        }
+
+    })
+
     test('Quick Links', async ({ page }) => {
         const links = ['Home', 'About Us', 'How it works', 'Contact Us', 'Gallery', 'Blog']
         for (let i of links) {
@@ -29,73 +40,68 @@ test.describe('Main Page', () => {
 
     })
 
-    test('Home link', async ({ page }) => {
-        const links = [
-            {
-                name: 'Home',
-                url: '/'
+    // test('Home link', async ({ page }) => {
+    //     const links = [
+    //         {
+    //             name: 'Home',
+    //             url: '/'
 
-            },
-            {
-                name: 'About Us',
-                url: '/about-us'
+    //         },
+    //         {
+    //             name: 'About Us ',
+    //             url: '/about-us'
 
-            },
-            {
-                name: 'How it Works',
-                url: '/how-it-work'
+    //         },
+    //         {
+    //             name: 'How it Works ',
+    //             url: '/how-it-work'
 
-            },
+    //         },
 
-            {
-                name: 'Contact Us',
-                url: '/contact'
-            },
+    //         {
+    //             name: 'Contact Us ',
+    //             url: '/contact'
+    //         },
 
-            {
-                name: 'Gallery',
-                url: '/gallery'
+    //         {
+    //             name: 'Gallery ',
+    //             url: '/gallery'
 
-            }
-        ]
-
-
-
-        for (let i of links) {
-
-            const linker = page.getByRole('link', { name: i.name }).first()
-
-            await linker.click()
-            await expect(page).toHaveURL(`https://findbanquet.com${i.url}`)
-            await page.goBack()
-            await page.waitForLoadState('domcontentloaded')
-        }
+    //         }
+    //     ]
 
 
 
+    //     for (let i of links) {
+    //         const linker = page.locator('link', { name: new RegExp(i.name) }).first()
+    //         await expect(linker).toBeVisible()
+    //         await linker.click()
+    //         await expect(page).toHaveURL(`https://findbanquet.com${i.url}`)
+    //         await page.waitForLoadState('domcontentloaded')
+    //         await page.goBack()
+    //     }
+    // })
+    test('About link', async ({ page }) => {
+        await page.getByRole('link', { name: 'About Us' }).click();
+        await expect(page).toHaveURL('https://findbanquet.com/about-us')
 
     })
-    // test('About link', async ({ page }) => {
-    //     await page.getByRole('link', { name: 'About Us' }).click();
-    //     await expect(page).toHaveURL('https://findbanquet.com/about-us')
 
-    // })
+    test('how it works link', async ({ page }) => {
+        await page.getByRole('link', { name: 'How it works' }).click();
+        await expect(page).toHaveURL('https://findbanquet.com/how-it-work')
 
-    // test('how it works link', async ({ page }) => {
-    //     await page.getByRole('link', { name: 'How it works' }).click();
-    //     await expect(page).toHaveURL('https://findbanquet.com/how-it-work')
+    })
 
-    // })
+    test('Contact us link', async ({ page }) => {
+        await page.getByRole('link', { name: 'Contact Us' }).click();
+        await expect(page).toHaveURL('https://findbanquet.com/contact')
 
-    // test('Contact us link', async ({ page }) => {
-    //     await page.getByRole('link', { name: 'Contact Us' }).click();
-    //     await expect(page).toHaveURL('https://findbanquet.com/contact')
+    })
 
-    // })
+    test('Gallery', async ({ page }) => {
+        await page.getByRole('link', { name: 'Gallery' }).click();
+        await expect(page).toHaveURL('https://findbanquet.com/gallery')
 
-    // test('Gallery', async ({ page }) => {
-    //     await page.getByRole('link', { name: 'Gallery' }).click();
-    //     await expect(page).toHaveURL('https://findbanquet.com/gallery')
-
-    // })
+    })
 })
