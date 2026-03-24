a6389177bce83b5d13951db529d6f29ae8b99186
brtester2025-collab <brtester2025@gmail.com>
2026-03-24 12:46:25 +0530

    Add Unit test for footer link section

--- tests/e2e/home_page.spec.js
+++ tests/e2e/home_page.spec.js
@@ -6,15 +6,11 @@ test.describe('Main Page', () => {
         await page.goto('https://findbanquet.com/');
     });
 
-
-    test.only('Searching the Shop', async ({ page }) => {
-
-        const serviceName = 'fish'
+    test('Searching the Shop', async ({ page }) => {
+        const serviceName = 'test'
         await page.getByRole('textbox').fill(serviceName)
         const suggestion = page.getByText(serviceName, { exact: true }).first();
         const noResult = page.getByText('No results found')
-
-
         if (await noResult.count() > 0) {
             await expect(noResult).toBeVisible();
         }
@@ -44,7 +40,7 @@ test.describe('Main Page', () => {
     })
 
 
-    test('Categories header section', async ({ page }) => {
+    test('Categories Header Section', async ({ page }) => {
         const categories = ['Mehendi', 'Makeup Artist', 'Makeup Artist', 'Banquet Hall', 'Fashion'
             , 'Resort', 'Hotel', 'Restaurants', 'Photographer', 'Decorators']
 
@@ -60,72 +56,46 @@ test.describe('Main Page', () => {
             const linker = page.getByRole('link', { name: i })
             await expect(linker).toBeVisible()
         }
-
-
     })
 
-    // test('Home link', async ({ page }) => {
-    //     const links = [
-    //         {
-    //             name: 'Home',
-    //             url: '/'
+    test('Footer Section Link', async ({ page }) => {
+        const links = [
+            {
+                name: 'Home',
+                url: '/'
 
-    //         },
-    //         {
-    //             name: 'About Us ',
-    //             url: '/about-us'
+            },
+            {
+                name: 'About Us',
+                url: '/about-us'
 
-    //         },
-    //         {
-    //             name: 'How it Works ',
-    //             url: '/how-it-work'
+            },
+            {
+                name: 'How it Works',
+                url: '/how-it-work'
 
-    //         },
+            },
 
-    //         {
-    //             name: 'Contact Us ',
-    //             url: '/contact'
-    //         },
+            {
+                name: 'Contact Us',
+                url: '/contact'
+            },
 
-    //         {
-    //             name: 'Gallery ',
-    //             url: '/gallery'
+            {
+                name: 'Gallery',
+                url: '/gallery'
 
-    //         }
-    //     ]
-
-
-
-    //     for (let i of links) {
-    //         const linker = page.locator('link', { name: new RegExp(i.name) }).first()
-    //         await expect(linker).toBeVisible()
-    //         await linker.click()
-    //         await expect(page).toHaveURL(`https://findbanquet.com${i.url}`)
-    //         await page.waitForLoadState('domcontentloaded')
-    //         await page.goBack()
-    //     }
-    // })
-    test('About link', async ({ page }) => {
-        await page.getByRole('link', { name: 'About Us' }).click();
-        await expect(page).toHaveURL('https://findbanquet.com/about-us')
-
-    })
-
-    test('how it works link', async ({ page }) => {
-        await page.getByRole('link', { name: 'How it works' }).click();
-        await expect(page).toHaveURL('https://findbanquet.com/how-it-work')
-
-    })
-
-    test('Contact us link', async ({ page }) => {
-        await page.getByRole('link', { name: 'Contact Us' }).click();
-        await expect(page).toHaveURL('https://findbanquet.com/contact')
-
-    })
-
-    test('Gallery', async ({ page }) => {
-        await page.getByRole('link', { name: 'Gallery' }).click();
-        await expect(page).toHaveURL('https://findbanquet.com/gallery')
-
+            }
+        ]
+        for (let i of links) {
+            const linker = page.getByRole('link', { name: (i.name) }).first()
+            await expect(linker).toBeVisible()
+            await linker.click()
+            await expect(page).toHaveURL(`https://findbanquet.com${i.url}`)
+            await page.goBack()
+            await page.goto('https://findbanquet.com');
+            await page.waitForLoadState('domcontentloaded')
+            await expect(page.getByRole('link', { name: 'Home' })).toBeVisible();
+        }
     })
 })
