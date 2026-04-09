daf4047512b4d29f6e1e339d7363a8a7a5616952
BRIJENDRA S BISHT <brijendrabisht1999@gmail.com>
2026-04-10 02:14:50 +0530

    pom object model

--- loop.js
+++ loop.js
@@ -21,3 +21,46 @@ for (let i = 0; i < ids.length; i++) {
 
     }
 }
+
+
+const users = [1, 2, 3, 4, 2, 5, 6, 3]   ///   2  3
+
+for (let i = 0; i < users.length; i++) {
+    for (let j = i + 1; j < users.length; j++)
+        if (users[i] === users[j])
+            console.log(users[i]);
+}
+
+
+const marks = [10, 80, 45, 99, 67, 100, 34]
+
+let highest = marks[0]
+
+for (let i = 0; i < marks.length; i++) {
+    if (marks[i] > highest) {
+        highest = marks[i]
+    }
+}
+console.log(highest);
+
+
+
+const user = [
+    { id: 1, status: "pass" },
+    { id: 2, status: "fail" },
+    { id: 3, status: "pass" },
+    { id: 4, status: "fail" }
+]
+
+let cnt = 0;
+
+for (let i = 0; i < user.length; i++) {
+    if (user[i].status === "fail") {
+        cnt++
+    }
+
+}
+console.log(cnt);
+
+
+
