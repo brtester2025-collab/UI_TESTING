b7688ad19df9125cc3e9b65dd518afa652a26343
BRIJENDRA S BISHT <brijendrabisht1999@gmail.com>
2026-04-15 01:49:54 +0530

    test

---
+++ test.js
@@ -0,0 +1,87 @@
+// const arr = [1, 2, 3, 4, 6];
+// let target = 6
+
+// let l = 0, r = arr.length - 1
+
+// while (l < r) {
+//     let sum = arr[l] + arr[r]
+
+//     if (sum === target) {
+//         console.log([l, r]);
+//         break;
+//     }
+
+//     else if (sum < target) {
+//         l++;
+//     }
+//     else {
+//         r--;
+//     }
+// }
+
+
+
+
+// const no = [10, 30, 50, 60, 70]
+// let st = 0;
+// let ed = no.length - 1
+// let tar = 80
+
+// while (st < ed) {
+//     let sum = no[st] + no[ed];
+
+//     if (sum === tar) {
+//         console.log([st, ed]);
+//         break;
+
+//     }
+
+//     else if (sum < tar) {
+//         st++;
+//     }
+
+//     else {
+//         ed--;
+//     }
+
+
+// }
+
+//Remove duplicate
+
+const arr = [1, 1, 2, 2, 3, 4, 4]
+let i = 0;
+let j = 1
+
+
+while (i < j) {
+
+
+    if (arr[i] != arr[j]) {
+        console.log([i]);
+
+
+    }
+
+    else {
+        i++;
+        j++;
+    }
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
