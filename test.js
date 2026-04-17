4b0dab68e8b28e797e49def03ff8c2793462bada
brtester2025-collab <brtester2025@gmail.com>
2026-04-17 15:31:33 +0530

    test

--- test.js
+++ test.js
@@ -113,11 +113,25 @@ for (let i = 0; i < n; i++) {
 
 
 
+let r = ""
+
+for (let i = 0; i < n; i++) {
+    for (let j = 0; j < i; j++) {
+        r += 'A'
+
+    }
+}
 
 
+let result = "";
 
-
-
+for (let i = 1; i <= 3; i++) {
+    result = "";
+    for (let j = 1; j <= i; j++) {
+        result += j;
+    }
+    console.log(result);
+}
 
 
 
