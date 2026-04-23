b3b1f968f818f2f1c5a387709c3536cc7445f024
brtester2025-collab <brtester2025@gmail.com>
2026-04-23 09:45:37 +0530

    test1

--- tests/backend/logic/q6.test.js
+++ tests/backend/logic/q6.test.js
@@ -1,62 +1,62 @@
-const { testing } = require('./q6')
+// const { testing } = require('./q6')
 
 
-describe('Estimation Damage and days', () => {
+// describe('Estimation Damage and days', () => {
 
-    test('no of days', () => {
-        const result = testing(7, true)
-        expect(result).toBe(true)
+//     test('no of days', () => {
+//         const result = testing(7, true)
+//         expect(result).toBe(true)
 
-    });
+//     });
 
-    test('no of days less than 7', () => {
-        const result = testing(6, true)
-        expect(result).toBe(true)
+//     test('no of days less than 7', () => {
+//         const result = testing(6, true)
+//         expect(result).toBe(true)
 
-    });
+//     });
 
-    test('no of days above 7', () => {
-        const result = testing(8, null)
-        expect(result).toBe(false)
-    })
+//     test('no of days above 7', () => {
+//         const result = testing(8, null)
+//         expect(result).toBe(false)
+//     })
 
-    test('no of days is -1', () => {
-        const result = testing(-1, true)
-        expect(result).toBe(false)
-    })
+//     test('no of days is -1', () => {
+//         const result = testing(-1, true)
+//         expect(result).toBe(false)
+//     })
 
-    test('no of days is undefined', () => {
-        const result = testing(undefined, true)
-        expect(result).toBe(false)
-    })
+//     test('no of days is undefined', () => {
+//         const result = testing(undefined, true)
+//         expect(result).toBe(false)
+//     })
 
-    test('no of days is empty', () => {
-        const result = testing(' ', true)
-        expect(result).toBe(false)
-    })
+//     test('no of days is empty', () => {
+//         const result = testing(' ', true)
+//         expect(result).toBe(false)
+//     })
 
-    test('no of days is decimal', () => {
-        const result = testing(1.4, true)
-        expect(result).toBe(false)
-    })
+//     test('no of days is decimal', () => {
+//         const result = testing(1.4, true)
+//         expect(result).toBe(false)
+//     })
 
-    test('no of days is null', () => {
-        const result = testing(null, true)
-        expect(result).toBe(false)
-    })
-    test('is Damaged is true', () => {
-        const result = testing(8, false)
-        expect(result).toBe(false)
-    })
+//     test('no of days is null', () => {
+//         const result = testing(null, true)
+//         expect(result).toBe(false)
+//     })
+//     test('is Damaged is true', () => {
+//         const result = testing(8, false)
+//         expect(result).toBe(false)
+//     })
 
-    test('isDamaged is false', () => {
-        const result = testing(8, null)
-        expect(result).toBe(false)
-    })
-    test('no of days less than 7 and isDamaged is false', () => {
-        const result = testing(6, false)
-        expect(result).toBe(true)
+//     test('isDamaged is false', () => {
+//         const result = testing(8, null)
+//         expect(result).toBe(false)
+//     })
+//     test('no of days less than 7 and isDamaged is false', () => {
+//         const result = testing(6, false)
+//         expect(result).toBe(true)
 
-    });
+//     });
 
-});
+// });
