18f6488a3fae66c9d27c000c6b1804d339972891
brtester2025-collab <brtester2025@gmail.com>
2026-05-01 09:46:23 +0530

    test1

--- tsdata/x2.ts
+++ tsdata/x2.ts
@@ -8,3 +8,15 @@ async function hello2() {
 
 console.log(hello());
 console.log(hello2());
+
+let user: { name: string; age: number } = { name: "john", age: 20 };
+
+function testsim(value: number): number {
+  return value;
+}
+
+//generics
+
+function ok<T>(value: T): T {
+  return value;
+}
