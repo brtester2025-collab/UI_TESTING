fc92f645f46a99a50922188d33c97f97638f8273
brtester2025-collab <brtester2025@gmail.com>
2026-05-25 12:17:01 +0530

    Add testCase of link verification

--- qa_playground/tc9.spec.ts
+++ qa_playground/tc9.spec.ts
@@ -23,7 +23,7 @@ test.skip('TC02: Verify link text matches expected label', async ({ page }) => {
   await expect(page.getByRole('link', { name: 'Home' })).toHaveText('Home');
 });
 
-test('TC03: Verify external link opens in a new tab', async ({ page }) => {
+test.skip('TC03: Verify external link opens in a new tab', async ({ page }) => {
   const externalLink = page.locator('a[target="_blank"]').nth(0);
   await expect(externalLink).toHaveAttribute('target', '_blank');
   await expect(externalLink).toHaveAttribute('rel', 'noopener noreferrer');
@@ -39,12 +39,25 @@ test('TC03: Verify external link opens in a new tab', async ({ page }) => {
   );
 });
 
-test('TC04: Verify internal link stays in the same tab', async ({ page }) => {
-  const links = page.locator('a[target = "_blank"]').nth(0);
-  await expect(links).toHaveAttribute('target', '_blank');
-  await expect(links).toHaveAttribute('target');
+test('TC04: Verify internal link stays in the same tab', async ({
+  page,
+  context,
+}) => {
+  const links = page.getByTestId('link-internal-home');
+  const CountPage = context.pages().length;
+
+  await links.click();
+  const href = await links.getAttribute('href');
+
+  const secondPage = context.pages().length;
+  await expect(secondPage).toBe(CountPage);
+  await expect(page).toHaveURL(new RegExp(`${href}`));
 });
 // test('', async ({ page }) => {});
 // test('', async ({ page }) => {});
 // test('', async ({ page }) => {});
 // test('', async ({ page }) => {});
+/*
+
+
+*/
