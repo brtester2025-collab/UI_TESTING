15007fda7866946663393c5dd8c8e284a81bf809
brtester2025-collab <brtester2025@gmail.com>
2026-05-29 15:36:00 +0530

    toc --11 for dynamic event

--- qa_playground/tc11.spec.ts
+++ qa_playground/tc11.spec.ts
@@ -22,3 +22,42 @@ test('TC01: Wait for a delayed browser alert to appear and accept it', async ({
     await dialog.accept();
   });
 });
+
+test('TC02: Wait for a hidden element to become visible after a delay', async ({
+  page,
+}) => {
+  await page.getByTestId('btn-show-element').click();
+  await page.waitForSelector('[data-testid="btn-show-element"]', {
+    state: 'visible',
+  });
+  await expect(page.getByTestId('delayed-element')).toBeVisible();
+});
+
+test('TC03: Wait for a disabled button to become enabled', async ({ page }) => {
+  const buttons = await page.getByTestId('btn-enable-after-delay');
+  await expect(buttons).toBeDisabled();
+
+  await page.getByTestId('btn-activate-trigger').click();
+  await expect(buttons).toBeEnabled();
+});
+
+test('TC04: Wait for loading text to change to a loaded state', async ({
+  page,
+}) => {
+  await page.getByTestId('btn-load-data').click();
+  await expect(page.getByTestId('load-status')).toHaveText('Data Loaded!');
+});
+
+test('TC05: Wait for a spinner to disappear before asserting completion', async ({
+  page,
+}) => {
+  const spinners = page.getByTestId('btn-start-spinner');
+  spinners.click();
+
+  const rounder = page.getByTestId('spinner');
+  await page.waitForSelector('rounder', { state: 'hidden' });
+  await expect(rounder).not.toBeVisible();
+  await expect(page.getByTestId('spinner-done')).toHaveText(
+    'Done! Spinner gone.'
+  );
+});
